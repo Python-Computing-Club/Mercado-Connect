97255092cacaac82be3414439246993ead934918
GabrielLimaA05 <160291817+GabrielLimaA05@users.noreply.github.com>
2025-09-08 15:30:41 -0300

    Update routes.js

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -2,11 +2,13 @@ import Home from "../pages/home";
 import Login from "../pages/login";
 import CadastroUsuario from "../pages/cadastroUsuario";
 import CadastroMercado from "../pages/cadastroMercado";
+import PainelMercado from './pages/painelMercado/index.js';
 
 export const publicRoutes = [
   { path: "/", element: <Login /> },
   { path: "/cadastro", element: <CadastroUsuario /> },
   { path: "/cadastro-parceiro", element: <CadastroMercado /> },
+  {path:"/painel-mercado", element: <PainelMercado/>}/>}
 ];
 
 export const privateRoutes = [{ path: "/home", element: <Home /> }];
